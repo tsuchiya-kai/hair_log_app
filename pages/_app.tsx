564564e75dd5e666101efcc5e199522e8883b1e3
tsuchiya-kai <yukaigangan@gmail.com>
2021-11-11 20:03:44 +0900

    basic認証の実装を完了

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,21 @@
-import "../styles/globals.scss";
+import "/styles/globals.scss";
+import App from "next/app";
+import type { AppProps, AppContext } from "next/app";
+import basicAuthCheck from "../lib/basicAuth"; //basic認証
 
-function MyApp({ Component, pageProps }) {
+console.log(basicAuthCheck);
+function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
+
+MyApp.getInitialProps = async (appContext: AppContext) => {
+  const { req, res } = appContext.ctx;
+  if (req && res) {
+    await basicAuthCheck(req, res);
+  }
+
+  const appProps = await App.getInitialProps(appContext);
+  return { ...appProps };
+};
 
 export default MyApp;
