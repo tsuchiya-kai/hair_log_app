d3f1470bcb8baf39433fcecbc46104e4d1b4925c
tsuchiya-kai <yukaigangan@gmail.com>
2021-11-11 21:57:28 +0900

    ITCSSの基盤の部分は使うことにした

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,8 @@ import styles from "../styles/pages/top-page.module.scss";
 export default function TopPage() {
   return (
     <div className={styles.topPage}>
+      <header className={styles.header}></header>
+      <h1 className={styles.title}>カタログを検索！</h1>
       <SearchIcon />
 
       {/* <div>トップです</div>
