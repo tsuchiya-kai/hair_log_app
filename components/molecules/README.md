6c21319d34370a96f87b3bc73279e52544b5b8c0
tsuchiya-kai <yukaigangan@gmail.com>
2021-11-11 21:07:07 +0900

    componentsディレクトリを追加

---
+++ components/molecules/README.md
@@ -0,0 +1,1 @@
+## atomic design の molecules 　に当たる component を格納します。
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import Link from "next/link";
+import Icon from "../components/atoms/icon/SearchIcon";
 
 export default function TopPage() {
   return (
     <>
+      <Icon />
       <div>トップです</div>
       <h1>h1テスト</h1>
       <h2>h2テスト</h2>
