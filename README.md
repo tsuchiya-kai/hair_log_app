805cd2f11b4c0fb89fb6571c06198883f5383565
tsuchiya-kai <yukaigangan@gmail.com>
2021-11-12 07:44:19 +0900

    readmeを更新

--- README.md
+++ README.md
@@ -6,8 +6,23 @@
 
 ## Environment
 
-- node ^^14.17.0
+- node ^14.17.0
+
   - vercel の都合でこのバージョンです
+
+- Next.js : 12.0.3
+- react : 17.0.2
+- typescript : ^4.4.4
+
+## Tools
+
+- ESLint
+- Prettier
+- stylelint
+- Sass
+  - CSS 設計
+    - CSS Modules
+    - ITCSS × RSCSS
 
 ## hosting
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,12 @@
 import "/styles/globals.scss";
 import App from "next/app";
 import type { AppProps, AppContext } from "next/app";
-import basicAuthCheck from "../lib/basicAuth"; //basic認証
+import basicAuthCheck from "../lib/basicAuth";
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
+// basic認証
 MyApp.getInitialProps = async (appContext: AppContext) => {
   const { req, res } = appContext.ctx;
   if (req && res) {
