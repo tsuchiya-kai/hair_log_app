81c49130f0b0fd50bd85803e288297f4a6d1c821
tsuchiya-kai <yukaigangan@gmail.com>
2021-11-12 21:45:02 +0900

    フッターを仮作成

--- components/organisms/TheFooter.tsx
+++ components/organisms/TheFooter.tsx
@@ -3,7 +3,20 @@ import styles from "styles/components/organisms/the-footer.module.scss";
 export default function TheFooter(props) {
   return (
     <header className={`${styles.theFooter} ${props.className}`}>
-      <div className={styles.inner}>仮のフッターです！</div>
+      <div className={styles.inner}>
+        <article className={styles.profile}>
+          <h2 className={styles.title}>Developer</h2>
+          <img
+            className={styles.image}
+            src="/images/profile.jpg"
+            alt="プロフィール画像"
+          />
+          <p className={styles.text}>Name: Tsuchiya Kaita</p>
+          <p className={styles.text}>Age:24</p>
+          <p className={styles.text}>Address: Shibuya</p>
+          <p className={styles.text}>Profession:Frontend Engineer</p>
+        </article>
+      </div>
     </header>
   );
 }
