fc2d0160b72d87fc0578288bc3abf3efe3c45ce0
tsuchiya-kai <yukaigangan@gmail.com>
2021-11-12 14:33:39 +0900

    css周りがぐちゃってるのでいったんコミット

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,11 +3,9 @@ import styles from "styles/pages/layout-container.module.scss";
 
 export default function Layout({ children }) {
   return (
-    <>
+    <div className={styles.layoutContainer}>
       <TheHeader />
-      <div className={styles.layoutContainer}>
-        <main>{children}</main>
-      </div>
-    </>
+      <main>{children}</main>
+    </div>
   );
 }
--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
-// const path = require("path");
+const path = require("path");
 // const { resolve } = path;
 
 const nextConfig = {
   reactStrictMode: true,
   // https://qiita.com/tatane616/items/e3ee99a181662ad6824b
-  webpack: (config) => config,
+  webpack: (config) => {
+    return config;
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+    prependData: `@import "ITCSS/settings/colors.scss";`,
+  },
 };
 
 module.exports = nextConfig;
