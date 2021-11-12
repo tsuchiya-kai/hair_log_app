2049e9742a5f477fb73982a3d12f4e3c57966b99
tsuchiya-kai <yukaigangan@gmail.com>
2021-11-12 21:49:38 +0900

    フッターの実装を完了

--- components/organisms/TheFooter.tsx
+++ components/organisms/TheFooter.tsx
@@ -1,6 +1,9 @@
+import dayjs from "dayjs";
 import styles from "styles/components/organisms/the-footer.module.scss";
 
 export default function TheFooter(props) {
+  const myBirthYear = 1998;
+  const age = dayjs().year() - myBirthYear;
   return (
     <header className={`${styles.theFooter} ${props.className}`}>
       <div className={styles.inner}>
@@ -12,7 +15,7 @@ export default function TheFooter(props) {
             alt="プロフィール画像"
           />
           <p className={styles.text}>Name: Tsuchiya Kaita</p>
-          <p className={styles.text}>Age:24</p>
+          <p className={styles.text}>Age:{age}</p>
           <p className={styles.text}>Address: Shibuya</p>
           <p className={styles.text}>Profession:Frontend Engineer</p>
         </article>
