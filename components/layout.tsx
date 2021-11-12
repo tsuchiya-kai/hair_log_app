780df3e6ba7d4973df9cd4eb3803a5a4a95b971a
tsuchiya-kai <yukaigangan@gmail.com>
2021-11-12 09:33:48 +0900

    aliasが効かない箇所に対しwork aroundする

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,4 +1,4 @@
-import { TheHeader } from "@components/organisms/index";
+import { TheHeader } from "../components/organisms/index"; // TODO: なぜかaliasが効かない
 import styles from "@styles/pages/layout-container.module.scss";
 
 export default function Layout({ children }) {
