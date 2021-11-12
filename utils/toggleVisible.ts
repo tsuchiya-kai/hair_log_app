7b370d28f72aded70c86b93dec745700dd5a195d
tsuchiya-kai <yukaigangan@gmail.com>
2021-11-12 18:31:48 +0900

    コメントを追加

--- utils/toggleVisible.ts
+++ utils/toggleVisible.ts
@@ -1,3 +1,10 @@
+/**
+ * TODO:
+ * CSSをある程度書いてから気づいたのだが、CSS ModuleとRSCSSを組み合わせると
+ * RSCSSにおけるvariantがうまく機能しないことに気づいた。
+ * そのため、本来variantに持たせるような責務をutilitiesで代用している。
+ * 時間がないためのwork aroundなのであとで見直すべき。
+ */
 export default function toggleVisible(flag: boolean) {
   return `_hidden ${flag ? "_show" : ""}`;
 }
