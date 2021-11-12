1d62c62273f5c14c54e11dafea02b18b9c80323a
tsuchiya-kai <yukaigangan@gmail.com>
2021-11-12 14:47:10 +0900

    work arounですが、scssの変数をglobalにしました

--- next.config.js
+++ next.config.js
@@ -3,13 +3,15 @@ const path = require("path");
 
 const nextConfig = {
   reactStrictMode: true,
-  // https://qiita.com/tatane616/items/e3ee99a181662ad6824b
+  // 参考: https://qiita.com/tatane616/items/e3ee99a181662ad6824b
   webpack: (config) => {
     return config;
   },
+  // NOTE: globalにscssの変数やmixinを効かせるための記述
+  // 参考: https://www.reddit.com/r/nextjs/comments/m1z72p/how_to_use_global_sass_variable/
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
-    prependData: `@import "global.scss";`,
+    prependData: `@import "globalVariables.scss";`,
   },
 };
 
