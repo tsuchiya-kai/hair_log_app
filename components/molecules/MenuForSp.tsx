dd91129ead90a1a6fd52a2341e3bff520af4721b
tsuchiya-kai <yukaigangan@gmail.com>
2021-11-13 14:30:06 +0900

    headerの機能を実装完了

--- components/molecules/MenuForSp.tsx
+++ components/molecules/MenuForSp.tsx
@@ -1,3 +1,5 @@
+import { useContext, useEffect } from "react";
+import { maskContext } from "components/layout";
 import styles from "styles/components/molecules/the-menu-for-sp.module.scss";
 
 type Props = {
@@ -11,14 +13,18 @@ export default function TheMenuForSp(props: Props) {
     isShow ? styles.Show : ""
   }`;
 
+  const { setMaskIsShow } = useContext(maskContext);
+
+  useEffect(() => {
+    setMaskIsShow(isShow);
+  }, [isShow]);
+
   return (
     <>
       <section {...rest} className={classNames}>
         <p>SPメニューの中身は</p>
         <p>遷移先が決まったら実装します！🙇‍♂️</p>
       </section>
-
-      {/* <div className={`${styles.mask} ${isShow ? styles.Show : ""}`} /> */}
     </>
   );
 }
