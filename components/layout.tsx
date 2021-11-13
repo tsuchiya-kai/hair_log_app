ec55a4166c9dcd6cf894dfbd838337419776e2de
tsuchiya-kai <yukaigangan@gmail.com>
2021-11-13 09:50:12 +0900

    toggleロジックを微修正

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,12 +1,7 @@
 import { TheHeader, TheFooter } from "components/organisms/index";
-import { useViewPort } from "hooks/useViewPort";
 import styles from "styles/pages/layout-container.module.scss";
 
 export default function Layout({ children }) {
-  const { windowWidth, windowHeight, clientHeight, maxScrollPosition } =
-    useViewPort();
-  console.log({ windowWidth, windowHeight, clientHeight, maxScrollPosition });
-
   return (
     <div className={styles.layoutContainer}>
       <TheHeader />
--- components/organisms/TheMenu.tsx
+++ components/organisms/TheMenu.tsx
@@ -2,21 +2,29 @@ import { useState } from "react";
 import { HamburgerMenuIcon } from "components/atoms/icon/index";
 import { MenuForPc } from "components/molecules/index";
 import toggleVisible from "utils/toggleVisible";
+// import { useViewPort } from "hooks/useViewPort";
 import styles from "styles/components/organisms/the-menu.module.scss";
 
 export default function TheMenu(props) {
   const [menuToggleState, setMenuToggleState] = useState<boolean>(false);
+
+  const toggleVisibleFlag = (e: Event) =>
+    setMenuToggleState(e.type === "mouseover");
+
+  // const { windowWidth } = useViewPort();
+  // const tabPortBreakPoint = 768;
+  // const isPcSize = windowWidth > tabPortBreakPoint;
   return (
     <div className={`${styles.theMenu} ${props.className}`}>
       <HamburgerMenuIcon
-        onMouseOver={() => setMenuToggleState(true)}
-        onMouseOut={() => setMenuToggleState(false)}
+        onMouseOver={toggleVisibleFlag}
+        onMouseOut={toggleVisibleFlag}
       />
 
       <MenuForPc
         className={`${styles.pc} ${toggleVisible(menuToggleState)}`}
-        onMouseOver={() => setMenuToggleState(true)}
-        onMouseOut={() => setMenuToggleState(false)}
+        onMouseOver={toggleVisibleFlag}
+        onMouseOut={toggleVisibleFlag}
       />
     </div>
   );
