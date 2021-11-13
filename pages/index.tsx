91620662ca8e6e02deec8a2b3a15d7c95bac7dd2
tsuchiya-kai <yukaigangan@gmail.com>
2021-11-13 16:12:50 +0900

    いったんコミット

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,43 @@
 // import Link from "next/link";
 import { SearchAnimationIcon } from "components/atoms/icon/index";
 import { AppInput } from "components/atoms/index";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from "styles/pages/top-page.module.scss";
 
 export default function TopPage() {
   const [inputState, setInputState] = useState("");
+
+  /**
+   * テストです
+   */
+  const [gitHubData, setGitHubData] = useState<any>({});
+  useEffect(() => {
+    const timeOutId = setTimeout(() => {
+      fetch(`https://api.github.com/users/${inputState}`)
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.message === "Not Found") {
+            console.log("ユーザーが存在しませんでした");
+            setGitHubData({
+              name: "ユーザーが存在しません！",
+              avatar_url:
+                "https://user-images.githubusercontent.com/24848110/33519396-7e56363c-d79d-11e7-969b-09782f5ccbab.png",
+            });
+            return;
+          }
+
+          setGitHubData(data);
+        })
+        .catch((e) => {
+          console.log("gitHubアカウントの取得に失敗しました", { e });
+        });
+    }, 1000);
+
+    return () => {
+      clearTimeout(timeOutId);
+    };
+  }, [inputState]);
+
   return (
     <div className={styles.topPage}>
       <h1 className={styles.title}>カタログを検索！</h1>
@@ -17,6 +49,12 @@ export default function TopPage() {
         state={inputState}
         onChange={(e) => setInputState(e.target.value)}
       />
+
+      <h2>検索結果：</h2>
+      <img src={gitHubData?.avatar_url} alt="" />
+      {Object.values(gitHubData).map((data, i) => (
+        <p key={i}>{data}</p>
+      ))}
     </div>
   );
 }
