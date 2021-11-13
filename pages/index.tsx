f9306270583ff890e86dbe7fa8ae4e42d2f566f5
tsuchiya-kai <yukaigangan@gmail.com>
2021-11-13 15:38:25 +0900

    inputのコンポーネントを作成

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,22 @@
 // import Link from "next/link";
-import { SearchIcon, SearchAnimationIcon } from "components/atoms/icon/index";
+import { SearchAnimationIcon } from "components/atoms/icon/index";
+import { AppInput } from "components/atoms/index";
+import { useState } from "react";
 import styles from "styles/pages/top-page.module.scss";
 
 export default function TopPage() {
+  const [inputState, setInputState] = useState("");
   return (
     <div className={styles.topPage}>
       <h1 className={styles.title}>カタログを検索！</h1>
-      <SearchIcon />
       <SearchAnimationIcon />
 
-      {/* <div>トップです</div>
-      <h1>h1テスト</h1>
-      <h2>h2テスト</h2>
-      <Link href="/">テスト</Link>
-      <li>
-        {[...Array(3)].map((_, index) => {
-          const temporaryId = index + 1;
-          return (
-            <Link href={`/beautician/${temporaryId}`} key={index}>
-              <a>美容師{temporaryId}</a>
-            </Link>
-          );
-        })}
-      </li> */}
+      <AppInput
+        className={styles.search}
+        placeholder="ヘアカタログを検索"
+        state={inputState}
+        onChange={(e) => setInputState(e.target.value)}
+      />
     </div>
   );
 }
