9d063150f6a52755ab3f923d2600ca0dea89f1b1
tsuchiya-kai <yukaigangan@gmail.com>
2021-11-14 10:11:44 +0900

    tsのstrictをtrueに変更

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,9 +5,12 @@ module.exports = {
     node: true,
   },
   extends: [
+    "next",
+    "next/core-web-vitals",
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
     "prettier",
   ],
   parser: "@typescript-eslint/parser",
@@ -17,6 +20,7 @@ module.exports = {
     },
     ecmaVersion: 12,
     sourceType: "module",
+    project: "./tsconfig.json",
   },
   plugins: ["react", "@typescript-eslint", "prettier"],
   rules: {
