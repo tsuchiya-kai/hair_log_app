b4735f09bbcf1ca096b08e80e0646b3ca09d895d
tsuchiya-kai <yukaigangan@gmail.com>
2021-11-17 23:32:38 +0900

    モーダルの実装を完了

--- components/organisms/TopPageModal.tsx
+++ components/organisms/TopPageModal.tsx
@@ -1,5 +1,6 @@
 import { AppModal } from "components/atoms/index";
 import styles from "styles/components/organisms/top-page-modal.module.scss";
+import innerStyles from "styles/components/organisms/top-page-modal-inner.module.scss";
 
 type Props = {
   data?: CatalogData;
@@ -14,16 +15,22 @@ export default function TopPageModal({ data, isShow, switchFunc }: Props) {
       switchFunc={switchFunc}
       isScroll
     >
-      <h3>{data?.beautician}</h3>
+      <div className={innerStyles.topPageModalInner}>
+        <h3 className={innerStyles.beautician}>{data?.beautician}</h3>
 
-      {data?.recent_posts.map((post, i) => {
-        return (
-          <div key={i + 1}>
-            <img src={post.url} alt={`投稿${i + 1}`} />
-            <p>{post.description}</p>
-          </div>
-        );
-      })}
+        {data?.recent_posts.map((post, i) => {
+          return (
+            <div className={innerStyles.imagewrap} key={i + 1}>
+              <img
+                className={innerStyles.image}
+                src={post.url}
+                alt={`投稿${i + 1}`}
+              />
+              <p className={innerStyles.description}>{post.description}</p>
+            </div>
+          );
+        })}
+      </div>
     </AppModal>
   );
 }
