5baaecb0634641a579e128866552a108ad92dceb
tsuchiya-kai <yukaigangan@gmail.com>
2021-11-18 16:02:21 +0900

    wipの場合に配置するコンポーネントを作成

--- components/organisms/index.ts
+++ components/organisms/index.ts
@@ -2,3 +2,4 @@ export { default as TheHeader } from "./TheHeader";
 export { default as TheFooter } from "./TheFooter";
 export { default as TheMenu } from "./TheMenu";
 export { default as TopPageModal } from "./TopPageModal";
+export { default as WIPSection } from "./WIPSection";
--- pages/post.tsx
+++ pages/post.tsx
@@ -1,3 +1,6 @@
+import { WIPSection } from "components/organisms/index";
+
 export default function Post() {
-  return <h1>投稿ページ</h1>;
+  const description = `こちらのページはログイン必須のページとして実装する予定です。\nユーザー登録した美容師が自分のカタログに投稿するための画面です。\nバリデーション実装等のformにまつわる技術をお見せする目的で制作することを予定しています。`;
+  return <WIPSection title="投稿ページ" description={description} />;
 }
