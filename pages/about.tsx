fae56d601dad12328aeaa3a9a6bf0158a7a4e1d7
tsuchiya-kai <yukaigangan@gmail.com>
2021-11-18 20:38:40 +0900

    aboutのmvを実装

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,8 +1,7 @@
 import Head from "next/head";
-import { WIPSection } from "components/organisms/index";
+import styles from "styles/pages/about-page.module.scss";
 
 export default function About() {
-  const description = `こちらのページはサービスの概要を説明するLPにする予定です。\nzozotownではGSAPを使っているようだったのでGSAPをキャッチアップして落とし込む予定です`;
   return (
     <>
       <Head>
@@ -17,10 +16,41 @@ export default function About() {
           content="美容師向けカタログ作成アプリ「ヘアログ」とは"
         />
       </Head>
-      <WIPSection
-        title="ヘアログとは（aboutページ）"
-        description={description}
-      />
+      <div className={styles.aboutPage}>
+        <section className={styles.mv}>
+          <div className={styles.mask} />
+          <h2 className={styles.title}>
+            なりたい自分を
+            <br />
+            より、見つけやすく。
+          </h2>
+          <img
+            className={styles.image}
+            src="/images/about_mv.png"
+            alt="メインビジュアル"
+          />
+        </section>
+
+        <section className={styles.description}>
+          <h1 className={styles.title}>HairLogとは</h1>
+        </section>
+
+        <section className={styles.forte}>
+          <div className={styles.flex}>
+            <div className={styles.left}>
+              <h2>美容師を検索</h2>
+            </div>
+            <div className={styles.right}></div>
+          </div>
+
+          <div className={styles.flex}>
+            <div className={styles.left}>
+              <h2>カテゴライズ</h2>
+            </div>
+            <div className={styles.right}></div>
+          </div>
+        </section>
+      </div>
     </>
   );
 }
