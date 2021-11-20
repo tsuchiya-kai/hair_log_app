51fb2069191c8ea7ff2b5f2fd8d86493c1aaae2d
tsuchiya-kai <yukaigangan@gmail.com>
2021-11-21 00:12:12 +0900

    ロゴをクリックした際の遷移でバグが生じているので応急処置

--- components/atoms/icon/SiteLogo.tsx
+++ components/atoms/icon/SiteLogo.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+// import Link from "next/link";
 import Urls from "lib/urls";
 import styles from "styles/components/atoms/site-icon.module.scss";
 
@@ -10,8 +10,13 @@ export default function SiteLogo(props: Props) {
   const { topPageUrl } = Urls;
 
   return (
+    // TODO: Linkを使って遷移すると、layoutなどが再描画されないためハンバーガーの状態が維持される
+    // そのため、遷移後も開きっぱなしになってしまうが
+    // TheMenuコンポーネントと等コンポーネントのstateを共有するには大幅な修正が必要なため
+    // 一旦は優先度を低く捉え、通常のaタグを用いた動的な遷移でページ全体を際描画することで対応する。
+    // 後ほど改修する。
     <div className={props.className ?? ""}>
-      <Link href={topPageUrl}>
+      <a href={topPageUrl}>
         <a className="_height100">
           <img
             className={`${styles.siteIcon}`}
@@ -19,7 +24,7 @@ export default function SiteLogo(props: Props) {
             alt="サイトロゴ"
           />
         </a>
-      </Link>
+      </a>
     </div>
   );
 }
